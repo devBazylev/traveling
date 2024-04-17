fadbd9f657f63083e9785d435dc0efaa54e4f569
jar <dev.bazylev@gmail.com>
2024-04-17 16:18:16 +0300

    header navigation-no-js update

--- source/js/header.js
+++ source/js/header.js
@@ -2,6 +2,12 @@ const header = document.querySelector('.header');
 const nav = header.querySelector('.header__navigation');
 const burger = header.querySelector('.header__button');
 
+nav.classList.remove('header__navigation--no-js');
+
+if(window.innerWidth < 1440){
+  nav.classList.add('header__navigation--closed');
+}
+
 const toggleMenu = () => {
   nav.classList.toggle('header__navigation--closed');
 };
