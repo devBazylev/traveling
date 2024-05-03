6d628f0cdc8d1c88f8a1b10fff7da14b3c9a345d
jar <dev.bazylev@gmail.com>
2024-05-03 19:42:42 +0300

    form full functionality

--- source/js/form.js
+++ source/js/form.js
@@ -1,17 +1,12 @@
 const form = document.querySelector('.form');
 const inputMail = form.querySelector('.form__mail');
 const inputTel = form.querySelector('.form__tel');
-// const submitButton = form.querySelector('.form__button');
+const submitButton = form.querySelector('.form__button');
 
-// const MAX_LETTERS_COUNT = 100;
+const MAX_LETTERS_COUNT = 50;
 
-// const toggleSubmitButton = (isDisabled) => {
-//   submitButton.disabled = isDisabled;
-// };
-
-const validateLetters = (mail) => {
-  const reg = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  return reg.test(String(mail));
+const toggleSubmitButton = (isDisabled) => {
+  submitButton.disabled = isDisabled;
 };
 
 const validateNumbers = (phone) => {
@@ -19,46 +14,48 @@ const validateNumbers = (phone) => {
   return reg.test(String(phone));
 };
 
-// const validateMail = () => {
-//   const mailValue = inputMail.value;
+const validateLetters = (mail) => {
+  const reg = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return reg.test(String(mail));
+};
 
-//   if (!validateLetters(mailValue)) {
-//     inputMail.classList.add('form__mail--error');
-//   } else {
-//     inputMail.classList.remove('form__mail--error');
-//   }
+const validateTel = (evt) => {
+  const telValue = inputTel.value;
 
-//   if (mailValue.length > MAX_LETTERS_COUNT) {
-//     inputMail.classList.add('form__mail--error');
-//   } else {
-//     inputMail.classList.remove('form__mail--error');
-//   }
-// };
+  if (!validateNumbers(telValue)) {
+    evt.preventDefault();
+    inputTel.classList.add('form__tel--error');
+  }
+  if (telValue.length > MAX_LETTERS_COUNT) {
+    evt.preventDefault();
+    inputTel.classList.add('form__tel--error');
+  }
+};
 
-// const validateTel = () => {
-//   const telValue = inputTel.value;
+const validateMail = (evt) => {
+  const mailValue = inputMail.value;
 
-//   if (!validateNumbers(telValue)) {
-//     inputTel.classList.add('form__tel--error');
-//   } else {
-//     inputTel.classList.remove('form__tel--error');
-//   }
-
-//   if (telValue.length > MAX_LETTERS_COUNT) {
-//     inputTel.classList.add('form__tel--error');
-//   } else {
-//     inputTel.classList.remove('form__tel--error');
-//   }
-// };
+  if (!validateLetters(mailValue)) {
+    evt.preventDefault();
+    inputMail.classList.add('form__mail--error');
+  }
+  if (mailValue.length > MAX_LETTERS_COUNT) {
+    evt.preventDefault();
+    inputMail.classList.add('form__mail--error');
+  }
+};
 
 form.addEventListener('submit', (evt) => {
-  if (!validateNumbers(inputTel.value)) {
-    inputTel.classList.add('form__tel--error');
-    evt.preventDefault();
-    // console.log('form__tel--error');
-  } else if (!validateLetters(inputMail.value)) {
-    inputMail.classList.add('form__mail--error');
-    evt.preventDefault();
-    // console.log('form__mail--error');
+  if (inputTel.classList.contains('form__tel--error')) {
+    inputTel.classList.remove('form__tel--error');
   }
+  if (inputMail.classList.contains('form__mail--error')) {
+    inputMail.classList.remove('form__mail--error');
+  }
+  toggleSubmitButton(true);
+  validateTel(evt);
+  validateMail(evt);
+  setTimeout(() => {
+    toggleSubmitButton(false);
+  }, 1000);
 });
