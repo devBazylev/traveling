811286a1a7f9ebe481eac8c511ef577e4c00aaec
jar <dev.bazylev@gmail.com>
2024-05-09 12:36:58 +0300

    gallery and adv sliders update

--- source/js/adv.js
+++ source/js/adv.js
@@ -10,34 +10,30 @@ if (window.innerWidth >= 1440) {
     const clone = slide.cloneNode(true);
     slider.appendChild(clone);
   });
-}
 
-const swiper = new Swiper('.adv', {
-  modules: [Navigation],
+  new Swiper('.adv', {
+    modules: [Navigation],
 
-  slidesPerView: 'auto',
-  slidesPerGroup: 2,
-  initialSlide: 2,
-  loop: true,
-  watchSlidesProgress: true,
-  watchOverflow: true,
-  centeredSlides: true,
-  loopAddBlankSlides: false,
-  loopAdditionalSlides: 0,
+    slidesPerView: 'auto',
+    slidesPerGroup: 2,
+    initialSlide: 2,
+    loop: true,
+    watchSlidesProgress: true,
+    watchOverflow: true,
+    centeredSlides: true,
+    loopAddBlankSlides: false,
+    loopAdditionalSlides: 0,
 
-  navigation: {
-    nextEl: '.adv__button--next',
-    prevEl: '.adv__button--prev',
-  },
-
-  breakpoints: {
-    1440: {
-      spaceBetween: 30,
-      simulateTouch: false,
+    navigation: {
+      nextEl: '.adv__button--next',
+      prevEl: '.adv__button--prev',
     },
-  }
-});
 
-if (window.innerWidth < 1440) {
-  swiper.destroy();
+    breakpoints: {
+      1440: {
+        spaceBetween: 30,
+        simulateTouch: false,
+      },
+    }
+  });
 }
--- source/js/gallery.js
+++ source/js/gallery.js
@@ -1,25 +1,32 @@
 import Swiper from 'swiper';
 import { Navigation } from 'swiper/modules';
 
-const swiper = new Swiper('.gallery', {
-  modules: [Navigation],
+const gallery = document.querySelector('.gallery');
+const slider = gallery.querySelector('.gallery__container');
+const slides = gallery.querySelectorAll('.gallery__slide');
 
-  watchSlidesProgress: true,
-  slideActiveClass: 'gallery__slide--active',
-  slideVisibleClass: 'gallery__slide--part',
-  // slideFullyVisibleClass: 'gallery__slide--visible',
+if (window.innerWidth < 1440) {
+  slides.forEach((slide) => {
+    const clone = slide.cloneNode(true);
+    slider.appendChild(clone);
+  });
 
-  spaceBetween: 5,
-  slidesPerView: 'auto',
-  slidesPerGroup: 1,
-  loop: true,
+  new Swiper('.gallery', {
+    modules: [Navigation],
 
-  navigation: {
-    nextEl: '.gallery__button--next',
-    prevEl: '.gallery__button--prev',
-  },
-});
+    watchSlidesProgress: true,
+    slideActiveClass: 'gallery__slide--active',
+    slideVisibleClass: 'gallery__slide--part',
 
-if (window.innerWidth > 1439) {
-  swiper.destroy();
+    spaceBetween: 5,
+    slidesPerView: 'auto',
+    slidesPerGroup: 1,
+    loop: true,
+    autoHeight: false,
+
+    navigation: {
+      nextEl: '.gallery__button--next',
+      prevEl: '.gallery__button--prev',
+    },
+  });
 }
