70e0734798f7e827b93153ca4071a5e328f3aac1
jar <dev.bazylev@gmail.com>
2024-05-12 15:13:19 +0300

    makes adv slider autocalc slides

--- source/js/adv.js
+++ source/js/adv.js
@@ -5,19 +5,27 @@ import { addClassArray, resetClassArray, cloneSlides } from './util.js';
 const mob = window.matchMedia('(min-width: 0px) and (max-width: 767px)');
 const tab = window.matchMedia('(min-width: 768px) and (max-width: 1439px)');
 const desk = window.matchMedia('(min-width: 1440px)');
-
 const adv = document.querySelector('.adv');
 const slider = adv.querySelector('.adv__container');
 const slides = adv.querySelectorAll('.adv__card');
+
+const slideWidth = parseFloat(getComputedStyle(slides[0]).width);
+const totalSlides = slides.length;
+const SLIDES_PER_GROUP = 2;
 const clones = [];
 
-cloneSlides(slider, slides, clones);
+const recalcSlides = () => {
+  const screenWidth = window.innerWidth;
+  const minSlides = Math.ceil(screenWidth / slideWidth + SLIDES_PER_GROUP);
+  for (let i = totalSlides; i < minSlides; i = i + totalSlides){
+    cloneSlides(slider, slides, clones);
+  }
+};
 
-new Swiper('.adv', {
+const swiper = new Swiper('.adv', {
   modules: [Navigation, Manipulation],
-  init: true,
+  init: false,
   loop: true,
-  watchSlidesProgress: true,
   observer: true,
   slidesPerView: 'auto',
   slidesPerGroup: 2,
@@ -59,6 +67,7 @@ new Swiper('.adv', {
         }, 300);
       }
       if (desk.matches) {
+        recalcSlides();
         this.enable();
         resetClassArray(clones, 'adv__card--none');
         setTimeout(() => {
@@ -69,28 +78,14 @@ new Swiper('.adv', {
   },
 });
 
-// const onScreen = () => {
-//   if (mob.matches) {
-//     swiper.disable();
-//     addClassArray(clones, 'adv__card--none');
-//     setTimeout(() => {
-//       slider.style.transform = 'translate3d(0px, 0px, 0px)';
-//     }, 300);
-//   }
-//   if (tab.matches) {
-//     swiper.disable();
-//     addClassArray(clones, 'adv__card--none');
-//     setTimeout(() => {
-//       slider.style.transform = 'translate3d(0px, 0px, 0px)';
-//     }, 300);
-//   }
-//   if (desk.matches) {
-//     slider.style.transform = 'translate3d(-1110px, 0px, 0px)';
-//     resetClassArray(clones, 'adv__card--none');
-//     swiper.enable();
-//     swiper.init();
-//   }
-// };
+const onScreen = () => {
+  if (totalSlides !== 0 && desk.matches) {
+    swiper.init();
+    window.removeEventListener('load', onScreen);
+    window.removeEventListener('resize', onScreen);
+  }
+};
 
-// window.addEventListener('load', onScreen);
-// window.addEventListener('resize', onScreen);
+window.addEventListener('load', onScreen);
+window.addEventListener('resize', onScreen);
+window.addEventListener('resize', recalcSlides);
